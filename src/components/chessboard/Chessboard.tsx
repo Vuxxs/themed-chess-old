9a3aba9e01a6cb9585c0552a27e58667d811a5bd
Vuxxs <vamvakaspanayiotis@gmail.com>
2022-04-15 02:09:25 +0300

    pieces code

--- src/components/chessboard/Chessboard.tsx
+++ src/components/chessboard/Chessboard.tsx
@@ -1,10 +1,53 @@
 import React from "react";
 import './Chessboard.css';
 
+const axisY = Array.from({ length: 9 }, (_, key) => key++).splice(1).reverse();
+const axisX = 'abcdefgh'.split('');
+
+function setPieces() {
+    // Starting FEN value for a chess game.
+    const FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR".split('');
+
+    const piecesDictionary: { [key: string]: string } = {
+        p: "pawn",
+        r: "rook",
+        n: "knight",
+        b: "bishop",
+        q: "queen",
+        k: "king"
+    };
+    const pieces = [];
+
+    let counterX = 0;
+    let counterY = 8;
+    for (const index in FEN) {
+
+        const place = FEN[index];
+        const pieceID = (axisX[counterX] + axisY[counterY]).toString()
+        counterX++;
+
+        if (place === "/") {
+            counterY--;
+            counterX = 8;
+        } else {
+            let pieceColor = "";
+            if (place === place.toUpperCase()) {
+                pieceColor = "white";
+            } else {
+                pieceColor = "black";
+            }
+
+            pieces.push(<a
+                className={pieceID}
+            >{piecesDictionary[place]}</a>
+            );
+        }
+    }
+    return pieces;
+}
+
 function setTiles() {
 
-    const axisY = Array.from({ length: 9 }, (_, key) => key++).splice(1);
-    const axisX = 'abcdefgh'.split('');
     const board = [];
     let leftSpacing = 0;
     let color;
@@ -21,8 +64,7 @@ function setTiles() {
                         left: `${leftSpacing}%`,
                         top: `${topSpacing}%`
                     }}
-                    className={`tiles ${color}`}
-                    id={tileID} />
+                    className={`tiles ${color} ${tileID}`} />
             );
 
             (color === "black") ? color = "white" : color = "black";
@@ -39,9 +81,11 @@ function setTiles() {
 
 export default function Chessboard() {
     const board = setTiles();
+    const pieces = setPieces();
     return (
         <div id="board">
             <a>{board}</a>
+            <a>{pieces}</a>
         </div>
     );
 }
